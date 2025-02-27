eefbf26bf1a4362cd0e6ce3ab7edddf8bf56c2e9
shinste <stephenshin1@hotmail.com>
2025-02-26 17:34:59 -0800

    update skills section

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,23 +2,27 @@ import useIntersectionAnimation from '../hooks/useAnimate';
 
 interface ImageProp {
 	image: string;
+	title: string;
 	rectangle?: boolean;
 	end?: boolean;
 	small?: boolean;
 }
 
-const Logo: React.FC<ImageProp> = ({ image, rectangle, end, small }) => {
+const Logo: React.FC<ImageProp> = ({ image, title, rectangle, end }) => {
 	useIntersectionAnimation();
-	if (small) {
-		return (
-			<div className="Logo animate" style={{ padding: 0 }}>
-				<img style={{ width: rectangle ? '1.5rem' : '3rem', height: '3rem' }} src={image} />
-			</div>
-		);
-	}
+	// if (small) {
+	// 	return (
+	// 		<div className="Logo animate" style={{ padding: 0 }}>
+	// 			<img style={{ width: rectangle ? '1.5rem' : '3rem', height: '3rem' }} src={image} />
+	// 		</div>
+	// 	);
+	// }
 	return (
-		<div className="Logo animate" style={{ padding: 10 }}>
+		<div className="Logo animate">
 			<img style={{ width: rectangle ? '4rem' : '5rem', height: '5rem' }} src={image} />
+			<p className="Center-text" style={{ fontSize: '15px' }}>
+				{title}
+			</p>
 		</div>
 	);
 };
--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -13,6 +13,8 @@ import postman from '../logos/postman.png';
 import github from '../logos/github.png';
 import aws from '../logos/aws.png';
 import postgresql from '../logos/postgresql.png';
+import nextjs from '../logos/nextjs.png';
+import firebase from '../logos/firebase.png';
 import Logo from '../components/Logo';
 import portrait from '../logos/portrait.jpg';
 import React from 'react';
@@ -27,116 +29,44 @@ const AboutMe: React.FC<AboutComponents> = ({ dayTime }) => {
 
 	return (
 		<div className="Page-div">
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				className="white display-block"
-				viewBox="0 0 1440 320">
-				<path
-					fill="#F9F9F9"
-					fill-opacity="1"
-					d="M0,32L48,37.3C96,43,192,53,288,48C384,43,480,21,576,21.3C672,21,768,43,864,69.3C960,96,1056,128,1152,160C1248,192,1344,224,1392,240L1440,256L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path>
-			</svg>
 			<div className="padding white">
-				<h3 className="main-header">TOOLS</h3>
-				<div className="Flex" style={{ width: '80vw' }}>
-					<p id="tech-text">
-						My go-to programming language is TypeScript, with React as my favorite library and
-						Next.js as my preferred framework. These tools have been primarily used throughout most
-						of my projects, allowing scalable, robust and user-focused applications. I’ve gained
-						much experience through utilizing these tools, and I am confident in utilizing them
-						effectively. I am also eager to learn any NEW tools/technologies to meet specific
-						project needs. I enjoy discovering new and innovative ways to enhance user experience
-						and delivering exceptional results.
-					</p>
-					<div className="tech-flex">
-						<div className="tech-hold">
-							<Logo image={typescript} />
-							<Logo image={python} rectangle={true} />
-							<Logo image={java} rectangle={true} />
-							<Logo image={javascript} />
-							<Logo image={html} />
-							<Logo image={r} />
+				<h3 className="main-header">SKILLS</h3>
+				<div className="flex-v pb-10">
+					<div>
+						<h2 className="Center-text">Languages</h2>
+						<div className="Flex justify-center">
+							<Logo image={typescript} title={'TypeScript'} />
+							<Logo image={python} title={'Python'} rectangle={true} />
+							<Logo image={java} title={'Java'} rectangle={true} />
+							<Logo image={javascript} title={'JavaScript'} />
+							<Logo image={html} title={'HTML'} />
+							<Logo image={r} title={'R'} />
 						</div>
-						<div className="tech-hold">
-							<Logo image={figma} />
-							<Logo image={postman} />
-							<Logo image={github} />
-							<Logo image={aws} />
-							<Logo image={postgresql} />
+					</div>
+					<div>
+						<h2 className="Center-text">Tools</h2>
+						<div className="Flex justify-center">
+							<Logo image={figma} title={'Figma'} />
+							<Logo image={postman} title={'Postman'} />
+							<Logo image={github} title={'Github'} />
+							<Logo image={aws} title={'AWS'} />
+							<Logo image={postgresql} title={'PostgreSQL'} />
+							<Logo image={firebase} title={'Firebase'} />
 						</div>
-						<div className="tech-hold">
-							<Logo image={react} />
-							<Logo image={django} />
-							<Logo image={flask} />
+					</div>
+
+					<div>
+						<h2 className="Center-text">Frameworks & Libraries</h2>
+						<div className="Flex justify-center">
+							<Logo image={nextjs} title={'NextJS'} />
+
+							<Logo image={react} title={'React'} />
+							<Logo image={react} title={'React Native'} />
+
+							<Logo image={django} title={'Django'} />
 						</div>
 					</div>
 				</div>
-				{/* <hr
-				style={{
-					width: '9rem',
-					borderColor: dayTime ? '#F5E186' : '#D9D9D9',
-					borderStyle: 'solid',
-					marginLeft: 0,
-					marginBottom: '2rem'
-				}}
-			/> */}
-
-				{/* <Typography m={1} sx={{ color: dayTime ? '#8B8B8B' : '#D9D9D9', marginLeft: 0 }}>
-				Languages
-			</Typography>
-			<div className="Tech-stack-holder">
-				<div
-					className="Tech-stack"
-					style={{
-						border: `4px solid ${dayTime ? '#F5E186' : '#969191'}`,
-						marginRight: '3rem'
-					}}>
-					<Logo image={typescript} />
-					<Logo image={python} rectangle={true} end={true} />
-				</div>
-				<div
-					className="Tech-stack"
-					style={{ border: `4px solid ${dayTime ? '#EEE5C7' : '#D9D9D9'}` }}>
-					<Logo image={java} rectangle={true} />
-					<Logo image={javascript} />
-					<Logo image={html} />
-					<Logo image={r} />
-				</div>
-			</div>
-
-			<Typography m={1} sx={{ color: dayTime ? '#8B8B8B' : '#D9D9D9', marginLeft: 0 }}>
-				Frameworks
-			</Typography>
-			<div className="Tech-stack-holder">
-				<div
-					className="Tech-stack"
-					style={{ border: `4px solid ${dayTime ? '#F5E186' : '#969191'}`, marginRight: '3rem' }}>
-					<Logo image={react} />
-					<div style={{ marginLeft: '-1rem' }}>
-						<Logo image={django} end={true} />
-					</div>
-				</div>
-				<Box
-					className="Flex"
-					sx={{ border: 1, borderWidth: 4, borderColor: dayTime ? '#EEE5C7' : '#D9D9D9' }}>
-					<Logo image={flask} end={true} />
-				</Box>
-			</div>
-
-			<Typography sx={{ color: dayTime ? '#8B8B8B' : '#D9D9D9' }}>Tools</Typography>
-			<div className="Tech-stack-holder">
-				<div
-					className="Tech-stack"
-					style={{ border: `4px solid ${dayTime ? '#F5E186' : '#969191'}` }}>
-					<Logo image={figma} />
-					<div style={{ marginLeft: '-1rem' }}>
-						<Logo image={postman} />
-					</div>
-					<Logo image={github} />
-					<Logo image={aws} />
-					<Logo image={postgresql} />
-				</div> */}
-				{/* </div> */}
 			</div>
 			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" className="white">
 				<path
